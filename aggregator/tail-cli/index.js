2fd4a5f38b532472993664036ef0aa20360eef7a
zaru <sakuraba@basicinc.jp>
2017-05-22 16:57:07 +0900

    Fixed to dispaly file log

--- aggregator/tail-cli/index.js
+++ aggregator/tail-cli/index.js
@@ -2,6 +2,16 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+let Tail = require('tail').Tail
+let colors = require('colors')
+let tail = new Tail("/tmp/remote_logs/remotetail.test1/log.sym")
+let ansiHTML = require('ansi-html');
+tail.on("line", function(line) {
+  let data = parse(line)
+  io.emit('chat message', ansiHTML(data.timestamp.gray + "\t" + data.tag.blue + "\t" + data.body.green))
+});
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -25,14 +35,14 @@ http.listen(3000, function(){
 //
 // tail = new Tail("/tmp/remote_logs/remotetail.test1/log.sym")
 //
-// function parse(line) {
-//   let words = line.split("\t")
-//   return {
-//     timestamp: words[0],
-//     tag: words[1],
-//     body: words[2]
-//   }
-// }
+function parse(line) {
+  let words = line.split("\t")
+  return {
+    timestamp: words[0],
+    tag: words[1],
+    body: words[2]
+  }
+}
 //
 // tail.on("line", function(line) {
 //   let data = parse(line)
